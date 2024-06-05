94aacbbe771c1d2bd3aab54bae29c61a09d4dca1
情野陸 <seinoriku@TEC-022-0042.local>
2024-06-05 18:09:06 +0900

    Add session helper test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -21,3 +21,14 @@ module ActiveSupport
     end
   end
 end
+
+class ActionDispatch::IntegrationTest
+
+  # テストユーザーとしてログインする
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
+end
+
