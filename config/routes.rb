643baaa6b2f9675b581e94108e0de0667c343a9c
情野陸 <--global>
2024-06-17 19:03:05 +0900

    Add resources of microposts to routing

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, onlt: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
   get '/signup', to: 'users#new'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
