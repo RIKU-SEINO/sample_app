827e76efbfe3ac8df6ff7cc4a5f1eec1dfb9abbe
情野陸 <--global>
2024-06-17 17:15:16 +0900

    Migrate micropost model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_17_045313) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_17_080600) do
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -26,4 +34,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_17_045313) do
     t.datetime "reset_sent_at"
   end
 
+  add_foreign_key "microposts", "users"
 end
