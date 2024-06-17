f1530ca79961a523e3c8ec300d1fcc5d2968b825
情野陸 <--global>
2024-06-17 13:53:40 +0900

    Remigrate user db

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   delete "/logout",  to: "sessions#destroy"
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, onlt: [:new, :create, :edit, :update]
   get '/signup', to: 'users#new'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
